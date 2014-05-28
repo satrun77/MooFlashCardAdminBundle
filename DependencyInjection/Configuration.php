b28b57ba4dc16065b2add36ce83653a43a6bf6ba
Mohamed Alsharaf <mohamed.alsharaf@gmail.com>
2014-05-28 14:26:39 +1200

    Fixed misc violations SensioLabsInsight

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -28,7 +28,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('moo_flash_card_admin');
+        $treeBuilder->root('moo_flash_card_admin');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
